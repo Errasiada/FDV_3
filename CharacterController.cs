91d51abe945753578595a6aad81ea88e4fc4040e
Noa Dionis <114673717+Errasiada@users.noreply.github.com>
2022-10-27 08:49:05 +0100

    Update CharacterController.cs

--- CharacterController.cs
+++ CharacterController.cs
@@ -6,22 +6,13 @@ public class CharacterController : MonoBehaviour
 {
     public float speed = 1f;
     
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // MOVEMENT
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         
-        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime, 0);
-        
-        // Utilizar Spacebar para incrementar velocidad
-
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime,0, verticalInput * speed * Time.deltaTime);
+     
         if (Input.GetKeyDown(KeyCode.Space))
         {
             speed *= 2;
